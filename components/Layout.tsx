eb21a0b0ce8d690197d88c7fbd6c5a7b32b112f9
hankei6km <hankei6km+git@gmail.com>
2020-10-19 22:16:30 +0900

    Fix type not matched

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,7 +19,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <AppBar color="default" position="static" elevation={false}>
+    <AppBar color="default" position="static" elevation={0}>
       <Toolbar>
         <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
         <Link href="/users">Users List</Link> |{' '}
