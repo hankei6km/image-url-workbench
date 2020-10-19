c19dd67758edbde480d20f68d9bc402ad1051f6e
hankei6km <hankei6km+git@gmail.com>
2020-10-19 22:41:39 +0900

    Make TabButton
    It will mainly used to switch each pages(tabs).

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,37 +1,84 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 // import Link from 'next/link';
-import Link from './Link';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
+import Link from './Link';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useTabButtonStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(1),
+    textTransform: 'none'
+  },
+  current: {
+    color: theme.palette.primary.main
+  }
+}));
+
+type TabButtonProps = {
+  label: string;
+  href: string;
+  naked?: boolean;
+  curTab?: boolean;
+};
+function TabButton({
+  label,
+  href,
+  naked = true,
+  curTab = false
+}: TabButtonProps) {
+  const classes = useTabButtonStyles();
+  return (
+    <Button className={classes.root} component={Link} naked={naked} href={href}>
+      <Typography className={curTab ? classes.current : ''} variant="h6">
+        {label}
+      </Typography>
+    </Button>
+  );
+}
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <AppBar color="default" position="static" elevation={0}>
-      <Toolbar>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </Toolbar>
-    </AppBar>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-);
+const Layout = ({ children, title = 'This is the default title' }: Props) => {
+  const router = useRouter();
+
+  useEffect(() => {}, [router]);
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <AppBar color="default" position="static" elevation={0}>
+        <Toolbar>
+          {[
+            { label: 'Home', href: '/' },
+            { label: 'About', href: '/about' },
+            { label: 'User List', href: '/users' },
+            {
+              label: 'Users API',
+              href: 'https://www.google.co.jp'
+            }
+          ].map((v, i) => (
+            <TabButton {...v} curTab={router.pathname === v.href} key={i} />
+          ))}
+        </Toolbar>
+      </AppBar>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  );
+};
 
 export default Layout;
--- components/Link.tsx
+++ components/Link.tsx
@@ -1,44 +1,56 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
 import NextLink, { LinkProps } from 'next/link';
 import MuiLink from '@material-ui/core/Link';
+import { UrlObject } from 'url';
 
-const NextComposed = React.forwardRef<typeof NextLink, LinkProps>(
-  function NextComposed(props, ref) {
-    // eslint-disable-next-line react/prop-types
-    const {
-      as,
-      href,
-      replace,
-      scroll,
-      passHref,
-      shallow,
-      prefetch,
-      ...other
-    } = props;
+const NextComposed = React.forwardRef<
+  typeof NextLink & HTMLAnchorElement,
+  LinkProps & { className?: string }
+>(function NextComposed(props, ref) {
+  const {
+    as,
+    href,
+    replace,
+    scroll,
+    passHref,
+    shallow,
+    prefetch,
+    ...other
+  } = props;
 
-    return (
-      <NextLink
-        href={href}
-        prefetch={prefetch}
-        as={as}
-        replace={replace}
-        scroll={scroll}
-        shallow={shallow}
-        passHref={passHref}
-      >
-        <a ref={ref} {...other} />
-      </NextLink>
-    );
-  }
-);
+  return (
+    <NextLink
+      href={href}
+      prefetch={prefetch}
+      as={as}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      passHref={passHref}
+    >
+      <a ref={ref} {...other} />
+    </NextLink>
+  );
+});
 
+type Props = {
+  children?: React.ReactNode;
+  activeClassName?: string;
+  as?: string | {};
+  className?: string;
+  href: string | (string & UrlObject);
+  innerRef?: any; // 降参。(node:any)=>void でもダメなの?
+  naked?: boolean;
+  onClick?: () => void;
+  prefetch?: boolean;
+};
 // A styled version of the Next.js Link component:
 // https://nextjs.org/docs/#with-link
-function Link(props) {
+function Link(props: LinkProps & Props) {
   const {
     href,
     activeClassName = 'active',
@@ -76,18 +88,19 @@ function Link(props) {
   );
 }
 
-Link.propTypes = {
-  activeClassName: PropTypes.string,
-  as: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  className: PropTypes.string,
-  href: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-  naked: PropTypes.bool,
-  onClick: PropTypes.func,
-  prefetch: PropTypes.bool
-};
+// Link.propTypes = {
+//   activeClassName: PropTypes.string,
+//   as: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+//   className: PropTypes.string,
+//   href: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+//   innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+//   naked: PropTypes.bool,
+//   onClick: PropTypes.func,
+//   prefetch: PropTypes.bool
+// };
 
 export default React.forwardRef<
   typeof Link,
+  //LinkProps & Props
   React.ComponentPropsWithoutRef<typeof Link>
 >((props, ref) => <Link {...props} innerRef={ref} />);
