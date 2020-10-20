667ef89f4e4efb1e1bd7b117d64b5e0f819d247b
hankei6km <hankei6km+git@gmail.com>
2020-10-21 01:12:43 +0900

    Add reducer to build params for image api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 import Layout from '../components/Layout';
 import Container from '@material-ui/core/Container';
 // import Typography from '@material-ui/core/Typography';
@@ -6,7 +6,34 @@ import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 
+type paramsState = [string, string][];
+
+const initialState: paramsState = [];
+function reducer(
+  state: paramsState,
+  action: { type: string; payload: [string, string] }
+): paramsState {
+  switch (action.type) {
+    case 'set':
+      let replaced = false;
+      const r: paramsState = state.map(([k, v]) => {
+        if (k === action.payload[0]) {
+          replaced = true;
+          return [k, action.payload[1]];
+        }
+        return [k, v];
+      });
+      if (!replaced) {
+        r.push(action.payload);
+      }
+      return r;
+    default:
+      throw new Error();
+  }
+}
+
 const IndexPage = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [urlText, setUrlText] = useState('');
   const [imgUrl, setImgUrl] = useState('');
 
@@ -14,7 +41,9 @@ const IndexPage = () => {
     let done = false;
     let id = setTimeout(() => {
       // setImgStat(inputText === '' ? 'none' : 'loading');
-      setImgUrl(urlText);
+      const q = new URLSearchParams('');
+      state.forEach(([k, v]) => q.append(k, v));
+      setImgUrl(`${urlText}?${q.toString()}`);
       done = true;
     }, 500);
     return () => {
@@ -22,7 +51,7 @@ const IndexPage = () => {
         clearTimeout(id);
       }
     };
-  }, [urlText]);
+  }, [urlText, state]);
 
   return (
     <Layout title="Home | Next.js + TypeScript Example">
@@ -46,6 +75,17 @@ const IndexPage = () => {
             fullWidth
             onChange={(e) => {
               setUrlText(e.target.value);
+            }}
+          />
+        </Box>
+        <Box p={1}>
+          <TextField
+            id="txt"
+            label="text"
+            defaultValue={''}
+            fullWidth
+            onChange={(e) => {
+              dispatch({ type: 'set', payload: ['txt', e.target.value] });
             }}
           />
         </Box>
