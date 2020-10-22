80797e26400c0cb3e89ad5488e735a6ef4177766
hankei6km <gh-hankei6km@outlook.jp>
2020-10-22 20:38:10 +0900

    Remove Box that surrounds TextField in ImgParams

--- components/ImgParams.tsx
+++ components/ImgParams.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+// import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import { paramsKeyToSpread } from '../utils/imgParamsUtils';
 
@@ -12,20 +12,12 @@ type ImgParamsProps = {
 
 export default function ImgParams({ paramsKey, onChange }: ImgParamsProps) {
   return (
-    <Box
-      p={1}
-      key={paramsKey}
-      display="flex"
-      flexDirection="row"
-      alignItems="center"
-    >
-      <TextField
-        variant="outlined"
-        id={paramsKey}
-        {...paramsKeyToSpread(paramsKey)}
-        fullWidth
-        onChange={(e) => onChange({ value: e.target.value })}
-      />
-    </Box>
+    <TextField
+      variant="outlined"
+      id={paramsKey}
+      {...paramsKeyToSpread(paramsKey)}
+      fullWidth
+      onChange={(e) => onChange({ value: e.target.value })}
+    />
   );
 }
