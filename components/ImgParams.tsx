b3a2497229a46a95e3cf537beaaeb96f1d9ddb25
hankei6km <gh-hankei6km@outlook.jp>
2020-10-23 13:31:10 +0900

    Make ImgParamsColor

--- components/ImgParams.tsx
+++ components/ImgParams.tsx
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 // import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Slider from '@material-ui/core/Slider';
 import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import { SketchPicker } from 'react-color';
 import {
   paramsKeyToSpread,
   paramsKeyToRange,
   paramsKeyIsColor
 } from '../utils/imgParamsUtils';
+
+const useStyles = makeStyles(() => ({
+  // root: {
+  //   '& > *': {
+  //     margin: theme.spacing(1),
+  //     width: '25ch'
+  //   }
+  // },
+  colorSample: {
+    width: 30,
+    height: 30,
+    backgroundColor: 'white'
+  }
+}));
 
 export type ImgUrlParamsOnChangeEvent = { value: string | number };
 
@@ -104,8 +122,59 @@ function ImgParamsRange({
   );
 }
 
-function ImgParamsColor() {
-  return <div />;
+function ImgParamsColor({ paramsKey, onChange }: ImgParamsProps) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [color, setColor] = useState('');
+  const [value, setValue] = useState('FF000000'); // ARGB
+
+  const p = paramsKeyToSpread(paramsKey);
+
+  // Button のラベルは後で調整
+  return (
+    <Box display="flex" flexDirection="row" alignItems="center">
+      <Box display="flex" alignItems="center">
+        <Button onClick={() => setOpen(true)}>{p.label}</Button>
+      </Box>
+      <Box px={1} display="flex" alignItems="center">
+        <svg onClick={() => setOpen(true)} className={classes.colorSample}>
+          <polygon points="0,0 10,0, 10,10 0,10 0,0" fill="black" />
+          <polygon points="10,10 20,10, 20,20 10,20 10,10" fill="black" />
+          <polygon points="20,20 30,20, 30,30 20,30 20,20" fill="black" />
+          <polygon points="20,0 30,0, 30,10 20,10 20,0" fill="black" />
+          <polygon points="0,20 10,20, 10,30 0,30 0,20" fill="black" />
+          <polygon
+            points="0,0 30,0, 30,30 0,30 0,0"
+            fill={`#${value.slice(2, 8)}${value.slice(0, 2)}`}
+          />
+        </svg>
+      </Box>
+      <Dialog
+        aria-labelledby={`color picker for ${p.label}`}
+        open={open}
+        onClose={() => {
+          setOpen(false);
+          onChange({ value: value });
+        }}
+        BackdropProps={{
+          invisible: true
+        }}
+      >
+        <SketchPicker
+          color={color}
+          onChange={(color: any) => {
+            console.log(color);
+            setColor(color.rgb);
+            setValue(
+              `${Math.floor(255 * color.rgb.a).toString(16)}${color.hex.slice(
+                1
+              )}`
+            );
+          }}
+        />
+      </Dialog>
+    </Box>
+  );
 }
 
 export default function ImgParams(props: ImgParamsProps) {
@@ -113,7 +182,7 @@ export default function ImgParams(props: ImgParamsProps) {
   if (suggestRange) {
     return <ImgParamsRange suggestRange={suggestRange} {...props} />;
   } else if (paramsKeyIsColor(props.paramsKey)) {
-    return <ImgParamsColor />;
+    return <ImgParamsColor {...props} />;
   }
   return <ImgParamsTextField {...props} />;
 }
