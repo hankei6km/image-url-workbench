230b66721a34634fb17334848918fdedeeb734b6
hankei6km <gh-hankei6km@outlook.jp>
2020-11-03 22:48:20 +0900

    Support cardTitle cardDescription

--- pages/card_gen.tsx
+++ pages/card_gen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import { GetStaticProps } from 'next';
 import {
   //GetStaticProps,
@@ -15,12 +15,16 @@ import TextField from '@material-ui/core/TextField';
 
 type CardGenPagePropsData = {
   imageUrl?: string;
+  cardTitle?: string;
+  cardDescription?: string;
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const data: CardGenPagePropsData = {
-      imageUrl: (context.query?.imageUrl as string) || ''
+      imageUrl: (context.query?.imageUrl as string) || '',
+      cardTitle: (context.query?.cardTitle as string) || '',
+      cardDescription: (context.query?.cardDescription as string) || ''
     };
     return { props: { ...data } };
   } catch (err) {
@@ -29,41 +33,83 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 const CardGenPage = ({
-  imageUrl
+  imageUrl,
+  cardTitle,
+  cardDescription
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   //const router = useRouter();
-  const [cardPreviewUrl, setCardPreviewUrl] = useState(imageUrl);
+  const [previewImageUrl, setPreviewImageUrl] = useState(imageUrl);
+  const [previewCardTitle, setPreviewCardTitle] = useState(cardTitle);
+  const [previewCardDescription, setPreviewCardDescription] = useState(
+    cardDescription
+  );
+  const [cardPreviewUrl, setCardPreviewUrl] = useState('');
 
   // console.log(router);
   // console.log(window.location);
 
-  // useEffect(() => {
-  //   setCardPreviewUrl(imageUrl);
-  // }, [imageUrl]);
+  useEffect(() => {
+    if (previewImageUrl) {
+      const q = new URLSearchParams('');
+      q.append('type', 'cardPreview');
+      q.append('imageUrl', previewImageUrl);
+      q.append('cardTitle', previewCardTitle);
+      q.append('cardDescription', previewCardDescription);
+      // setCardPreviewUrl(`${window.location.href}?${q.toString()}`);
+      console.log(window.location);
+      console.log(window.location.href.split('?', 1)[0]);
+      console.log(q.toString());
+      setCardPreviewUrl(
+        `${window.location.href.split('?', 1)[0]}?${q.toString()}`
+      );
+    } else {
+      setCardPreviewUrl('');
+    }
+  }, [previewImageUrl, previewCardTitle, previewCardDescription]);
 
   return (
     <Layout title="Card Gen">
       <Head>
-        <meta name="description" content="preview card description" />
+        <meta name="og:description" content={cardDescription} />
         <meta property="og:image" content={imageUrl} />
-        <meta name="og:title" content="card preview title" />
+        <meta name="og:title" content={cardTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Container maxWidth="sm">
-        <Box p={1}>
-          <TextField
-            id="image-url"
-            label="Image URL"
-            defaultValue={''}
-            fullWidth
-            onChange={(e) => {
-              const q = new URLSearchParams('');
-              q.append('type', 'cardPreview');
-              q.append('imageUrl', e.target.value);
-              // setCardPreviewUrl(`${window.location.href}?${q.toString()}`);
-              setCardPreviewUrl(`${window.location.href}?${q.toString()}`);
-            }}
-          />
+        <Box pb={3}>
+          <Box p={1}>
+            <TextField
+              id="preview-image-url"
+              label="Preview Image URL"
+              defaultValue={imageUrl}
+              fullWidth
+              onChange={(e) => {
+                setPreviewImageUrl(e.target.value);
+              }}
+            />
+          </Box>
+          <Box p={1}>
+            <TextField
+              id="preview-card-title"
+              label="Preview Card Title"
+              defaultValue={cardTitle}
+              fullWidth
+              onChange={(e) => {
+                setPreviewCardTitle(e.target.value);
+              }}
+            />
+          </Box>
+          <Box p={1}>
+            <TextField
+              id="preview-card-description"
+              label="Preview Card Description"
+              defaultValue={cardDescription}
+              fullWidth
+              onChange={(e) => {
+                setPreviewCardDescription(e.target.value);
+              }}
+            />
+          </Box>
         </Box>
         <Box p={1}>
           <TextField
