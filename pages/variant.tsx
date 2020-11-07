4074d39aa48e6b61755bba1cecc6008b32dd958a
hankei6km <gh-hankei6km@outlook.jp>
2020-11-08 02:56:18 +0900

    Render image as md / html

--- pages/variant.tsx
+++ pages/variant.tsx
@@ -1,23 +1,52 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ReactDomServer from 'react-dom/server';
 import Layout from '../components/Layout';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
+// import TurndownService from 'turndown';
 import PreviewContext from '../components/PreviewContext';
 
+// const turndownService = new TurndownService();
+
+const markdwonEscapeRegExp = /([[\]\\<>])/g;
 const VariantPage = () => {
   const previewStateContext = useContext(PreviewContext);
   //const previewDispatch = useContext(PreviewDispatch);
-  const [value] = useState(`![](${previewStateContext.previewImageUrl})`);
+  const [altText, setAltText] = useState('');
+  const [markdown, setMarkdown] = useState('');
+  const [imgElement, setImgElement] = useState(
+    <img src={previewStateContext.previewImageUrl} alt="" />
+  );
 
-  const imgElement = <img src={previewStateContext.previewImageUrl} alt="" />;
+  useEffect(() => {
+    // markdown は手動でエスケープでも良いのでは?
+    setMarkdown(
+      `![${altText.replace(markdwonEscapeRegExp, '\\$1')}](${
+        previewStateContext.previewImageUrl
+      })`
+    );
+  }, [previewStateContext.previewImageUrl, altText]);
+
+  useEffect(() => {
+    setImgElement(
+      <img src={previewStateContext.previewImageUrl} alt={altText} />
+    );
+  }, [previewStateContext.previewImageUrl, altText]);
 
   return (
     <Layout title="Variant">
       <Container maxWidth="sm">
         <Box>
-          <TextField fullWidth multiline value={value} />
+          <TextField
+            fullWidth
+            multiline
+            value={altText}
+            onChange={(e) => setAltText(e.target.value)}
+          />
+        </Box>
+        <Box>
+          <TextField fullWidth multiline value={markdown} />
         </Box>
         <Box>
           <TextField
