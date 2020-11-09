2e4050f8e4cdf45b4172f0836ecde29cb26caf74
hankei6km <gh-hankei6km@outlook.jp>
2020-11-09 13:46:37 +0900

    Use unified to generate variant source fragment
    * VariantMarkdown was removed.

--- pages/variant.tsx
+++ pages/variant.tsx
@@ -4,43 +4,129 @@ import Layout from '../components/Layout';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
-// import TurndownService from 'turndown';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import unified from 'unified';
+import rehypeParse from 'rehype-parse';
+import rehypeStringify from 'rehype-stringify';
+import rehypeToRemark from 'rehype-remark';
+import remarkStringify from 'remark-stringify';
+import rehypeSanitize from 'rehype-sanitize';
 import PreviewContext from '../components/PreviewContext';
-import VariantMarkdown from '../components/VariantMarkdown';
+import reporter from 'vfile-reporter';
+// import VariantMarkdown from '../components/VariantMarkdown';
+
+const processorHtml = unified()
+  .use(rehypeParse, { fragment: true })
+  .use(rehypeSanitize)
+  .use(rehypeStringify)
+  .freeze();
+
+const processorMarkdown = unified()
+  .use(rehypeParse, { fragment: true })
+  .use(rehypeSanitize)
+  .use(rehypeToRemark)
+  .use(remarkStringify)
+  .freeze();
 
 const VariantPage = () => {
   const previewStateContext = useContext(PreviewContext);
   //const previewDispatch = useContext(PreviewDispatch);
   const [altText, setAltText] = useState('');
-  const [imgElement, setImgElement] = useState(
-    <img src={previewStateContext.previewImageUrl} alt="" />
-  );
+  const [linkText, setLinkText] = useState('');
+  const [newTab, setNewTab] = useState(false);
+  const [imgHtml, setImgHtml] = useState('');
+  const [imgMarkdown, setImgMarkdown] = useState('');
 
   useEffect(() => {
-    setImgElement(
-      <img src={previewStateContext.previewImageUrl} alt={altText} />
+    const imgElement = (
+      <img
+        src={previewStateContext.previewImageUrl}
+        alt={altText}
+        title={altText}
+      />
     );
-  }, [previewStateContext.previewImageUrl, altText]);
+    const t = newTab
+      ? {
+          target: '_blank',
+          rel: 'noopener noreferrer'
+        }
+      : {};
+    const elm = linkText ? (
+      <a href={linkText} {...t}>
+        {imgElement}
+      </a>
+    ) : (
+      imgElement
+    );
+    const html = ReactDomServer.renderToStaticMarkup(elm);
+    processorHtml.process('<div abc</div>', (err, file) => {
+      if (err) {
+        console.error(reporter(err || file));
+      }
+      setImgHtml(String(file));
+    });
+    processorMarkdown.process(html, (err, file) => {
+      if (err) {
+        console.error(reporter(file)); // 今回の利用ではここでデバッグ用の情報は表示されなさそう
+      }
+      setImgMarkdown(String(file).trimEnd());
+    });
+  }, [previewStateContext.previewImageUrl, altText, linkText, newTab]);
 
   return (
     <Layout title="Variant">
       <Container maxWidth="sm">
-        <Box>
-          <VariantMarkdown />
-        </Box>
-        <Box>
+        <Box p={1}>
           <TextField
+            label="alt text"
             fullWidth
-            multiline
             value={altText}
             onChange={(e) => setAltText(e.target.value)}
           />
         </Box>
-        <Box>
+        <Box p={1} display="flex" flexDirection="row">
+          <Box flexGrow={1} mr={1}>
+            <TextField
+              label="link"
+              fullWidth
+              value={linkText}
+              onChange={(e) => setLinkText(e.target.value)}
+            />
+          </Box>
+          <Box>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={newTab}
+                  onChange={(e) => {
+                    setNewTab(e.target.checked);
+                  }}
+                  color="primary"
+                  name="newTab"
+                  inputProps={{ 'aria-label': `switch open link in new tab` }}
+                />
+              }
+              label="new tab"
+            />
+          </Box>
+        </Box>
+        <Box p={1}>
           <TextField
+            label="html"
+            variant="outlined"
             fullWidth
             multiline
-            value={ReactDomServer.renderToStaticMarkup(imgElement)}
+            value={imgHtml}
+          />
+        </Box>
+        <Box p={1}>
+          <TextField
+            label="markdown"
+            variant="outlined"
+            fullWidth
+            multiline
+            value={imgMarkdown}
           />
         </Box>
       </Container>
