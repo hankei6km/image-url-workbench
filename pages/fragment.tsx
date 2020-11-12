beb40f97f9588ee9b4bfc58f284fd7cc59b99acd
hankei6km <gh-hankei6km@outlook.jp>
2020-11-12 16:08:42 +0900

    initial commit

--- pages/fragment.tsx
+++ pages/fragment.tsx
@@ -1,5 +1,17 @@
-import React, { useState, useContext, useEffect, useRef } from 'react';
+import React, {
+  useCallback,
+  useState,
+  useContext,
+  useEffect,
+  useRef
+} from 'react';
 import ReactDomServer from 'react-dom/server';
+import {
+  //GetStaticProps,
+  InferGetServerSidePropsType,
+  GetServerSideProps
+} from 'next';
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
@@ -12,8 +24,12 @@ import rehypeStringify from 'rehype-stringify';
 import rehypeToRemark from 'rehype-remark';
 import remarkStringify from 'remark-stringify';
 import rehypeSanitize from 'rehype-sanitize';
-import PreviewContext from '../components/PreviewContext';
+import Validator from '../utils/validator';
+import PreviewContext, { PreviewDispatch } from '../components/PreviewContext';
 import DebTextField from '../components/DebTextField';
+import { encodeBase64Url, decodeBase64Url } from '../utils/base64';
+
+const validator = Validator();
 
 const processorHtml = unified()
   .use(rehypeParse, { fragment: true })
@@ -28,9 +44,30 @@ const processorMarkdown = unified()
   .use(remarkStringify)
   .freeze();
 
-const FragmentPage = () => {
+type FragmentPagePropsData = {
+  imageUrl?: string;
+  title?: string;
+  description?: string;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const data: FragmentPagePropsData = {
+      imageUrl: decodeBase64Url((context.query?.imageUrl as string) || ''),
+      title: decodeBase64Url((context.query?.title as string) || ''),
+      description: decodeBase64Url((context.query?.description as string) || '')
+    };
+    return { props: { data: data } };
+  } catch (err) {
+    return { props: { errors: err.message } };
+  }
+};
+
+const FragmentPage = ({
+  data
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const previewStateContext = useContext(PreviewContext);
-  //const previewDispatch = useContext(PreviewDispatch);
+  const previewDispatch = useContext(PreviewDispatch);
   const [altText, setAltText] = useState('');
   const [linkText, setLinkText] = useState('');
   const [newTab, setNewTab] = useState(false);
@@ -38,6 +75,21 @@ const FragmentPage = () => {
   const [imgMarkdown, setImgMarkdown] = useState('');
   const htmlInputRef = useRef<HTMLTextAreaElement>();
   const markdownInputRef = useRef<HTMLTextAreaElement>();
+
+  const [imageUrl, setImageUrl] = useState(
+    data.imageUrl || previewStateContext.previewImageUrl // assets のチェックが入らない状態になる. あとで対応
+  );
+  const [imageUrlErrMsg, setImageUrlErrMsg] = useState('');
+  const [title, setTitle] = useState(data.cardTitle || '');
+  const [description, setDescription] = useState(data.cardDescription || '');
+  const [cardPreviewUrl, setCardPreviewUrl] = useState('');
+
+  const dataImageUrlErr = validator.assets(
+    data.imageUrl,
+    previewStateContext.validateAssets,
+    previewStateContext.assets,
+    true
+  );
 
   useEffect(() => {
     const imgElement = (
@@ -71,9 +123,85 @@ const FragmentPage = () => {
     });
   }, [previewStateContext.previewImageUrl, altText, linkText, newTab]);
 
+  const validateImageUrl = useCallback(() => {
+    const err = validator.assets(
+      imageUrl,
+      previewStateContext.validateAssets,
+      previewStateContext.assets,
+      true
+    );
+    if (err && imageUrl !== '') {
+      return err.message;
+    }
+    return '';
+  }, [
+    previewStateContext.validateAssets,
+    previewStateContext.assets,
+    imageUrl
+  ]);
+
+  useEffect(() => {
+    setImageUrlErrMsg(validateImageUrl());
+  }, [validateImageUrl, imageUrl]);
+
+  useEffect(() => {
+    if (imageUrl && validateImageUrl() === '') {
+      const q = new URLSearchParams('');
+      q.append('type', 'cardPreview');
+      q.append('imageUrl', encodeBase64Url(imageUrl));
+      q.append('title', encodeBase64Url(title));
+      q.append('description', encodeBase64Url(description));
+      // setCardPreviewUrl(`${window.location.href}?${q.toString()}`);
+      setCardPreviewUrl(
+        `${window.location.href.split('?', 1)[0]}?${q.toString()}`
+      );
+    } else {
+      setCardPreviewUrl('');
+    }
+  }, [imageUrl, title, description, validateImageUrl]);
+
+  useEffect(() => {
+    if (validateImageUrl() === '') {
+      previewDispatch({
+        type: 'setPreviewImageUrl',
+        payload: [imageUrl]
+      });
+    }
+  }, [previewDispatch, imageUrl, validateImageUrl]);
+
   return (
     <Layout title="Fragment">
+      <Head>
+        <meta
+          name="og:description"
+          content={
+            dataImageUrlErr === undefined
+              ? data.description || '[preview] description'
+              : dataImageUrlErr.message
+          }
+        />
+        <meta
+          property="og:image"
+          content={dataImageUrlErr === undefined ? data.imageUrl : ''}
+        />
+        <meta name="og:title" content={data.title || '[preview] title'} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <Container maxWidth="sm">
+        <Box p={1}>
+          <DebTextField
+            error={imageUrlErrMsg ? true : false}
+            id="preview-card-image-url"
+            label="Preview Card Image URL"
+            defaultValue={imageUrl}
+            fullWidth
+            helperText={imageUrlErrMsg}
+            // 入力できないようにする?
+            onChangeValue={({ value }) => {
+              setImageUrl(value);
+            }}
+          />
+        </Box>
         <Box p={1}>
           <DebTextField
             label="alt text"
@@ -137,6 +265,42 @@ const FragmentPage = () => {
               }
             }}
           />
+        </Box>
+        <Box>
+          <Box>
+            <Box p={1}>
+              <DebTextField
+                id="preview-card-title"
+                label="Preview Card Title"
+                defaultValue={data.cardTitle}
+                fullWidth
+                onChangeValue={({ value }) => {
+                  setTitle(value);
+                }}
+              />
+            </Box>
+            <Box p={1}>
+              <DebTextField
+                id="preview-card-description"
+                label="Preview Card Description"
+                defaultValue={data.cardDescription}
+                fullWidth
+                onChangeValue={({ value }) => {
+                  setDescription(value);
+                }}
+              />
+            </Box>
+          </Box>
+          <Box p={1}>
+            <TextField
+              id="card-preview-url"
+              label="Card Preview URL"
+              //defaultValue={''}
+              value={cardPreviewUrl}
+              fullWidth
+              //onChange={debounceBaseUrl()}
+            />
+          </Box>
         </Box>
       </Container>
     </Layout>
