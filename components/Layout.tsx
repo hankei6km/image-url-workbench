8bc17d08e1927197f8022543067dd79eeec30c57
hankei6km <gh-hankei6km@outlook.jp>
2020-11-14 07:08:15 +0900

    Use Tabs component to head in Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,45 +1,12 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode } from 'react';
 // import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Link from './Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-
-const useTabButtonStyles = makeStyles((theme) => ({
-  root: {
-    margin: theme.spacing(1),
-    textTransform: 'none'
-  },
-  current: {
-    color: theme.palette.primary.main
-  }
-}));
-
-type TabButtonProps = {
-  label: string;
-  href: string;
-  naked?: boolean;
-  curTab?: boolean;
-};
-function TabButton({
-  label,
-  href,
-  naked = true,
-  curTab = false
-}: TabButtonProps) {
-  const classes = useTabButtonStyles();
-  return (
-    <Button className={classes.root} component={Link} naked={naked} href={href}>
-      <Typography className={curTab ? classes.current : ''} variant="h6">
-        {label}
-      </Typography>
-    </Button>
-  );
-}
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -50,15 +17,30 @@ const useStyles = makeStyles((theme) => ({
     },
     '& > .MuiPaper-root': {
       position: 'static',
-      overflowX:'auto',
+      flexGrow: 1,
+      width: '100%',
       [theme.breakpoints.up('lg')]: {
         position: 'sticky',
         top: 0,
         zIndex: theme.zIndex.appBar
+      },
+      '& .MuiTab-root': {
+        textTransform: 'none',
+        [theme.breakpoints.up('lg')]: {
+          minWidth: 120
+        }
       }
     }
   }
 }));
+
+const tabLink = [
+  { label: 'Home', href: '/' },
+  { label: 'Render', href: '/render' },
+  { label: 'Card', href: '/card_gen' },
+  { label: 'Fragment', href: '/fragment' },
+  { label: 'About', href: '/about' }
+];
 
 type Props = {
   children?: ReactNode;
@@ -69,7 +51,11 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => {
   const router = useRouter();
   const classes = useStyles();
 
-  useEffect(() => {}, [router]);
+  const tabValue = (asPath: string) => {
+    const p = asPath.split('?', 1)[0];
+    return tabLink.findIndex((v) => v.href === p);
+  };
+
   return (
     <div className={classes.root}>
       <Head>
@@ -77,18 +63,18 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Paper elevation={1}>
-        <Toolbar variant="dense">
-          {[
-            { label: 'Home', href: '/' },
-            { label: 'Render', href: '/render' },
-            { label: 'Card', href: '/card_gen' },
-            { label: 'Fragment', href: '/fragment' },
-            { label: 'About', href: '/about' }
-          ].map((v, i) => (
-            <TabButton {...v} curTab={router.pathname === v.href} key={i} />
+      <Paper square elevation={1}>
+        <Tabs
+          indicatorColor="primary"
+          textColor="primary"
+          centered // TODO: スクロールで中央寄せ
+          variant="scrollable"
+          value={tabValue(router.asPath)}
+        >
+          {tabLink.map((v, i) => (
+            <Tab {...v} key={i} component={Link} naked />
           ))}
-        </Toolbar>
+        </Tabs>
       </Paper>
       <div>{children}</div>
       <footer>
