92ea5cd677c0b56888f0a4c126397e2da7189ac0
hankei6km <gh-hankei6km@outlook.jp>
2020-11-24 04:00:23 +0900

    Make FragmentLink

--- pages/fragment.tsx
+++ pages/fragment.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React from 'react';
 import Layout from '../components/Layout';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import PreviewContext from '../components/PreviewContext';
-import FragmentTextField from '../components/FragmentTextField';
+import FragmentLink from '../components/FragmentLink';
 import FragmentParams from '../components/FragmentParams';
 import FragmentTag from '../components/FragmentTag';
 
@@ -29,38 +28,12 @@ export function FragmentPanel({
 }
 
 const FragmentPage = () => {
-  const previewStateContext = useContext(PreviewContext);
-  const [imgUrl, setImgUrl] = useState('');
-  const [imgPath, setImgPath] = useState('');
-
-  useEffect(() => {
-    try {
-      const tmpImgUrl: string[] = [];
-      const tmpImgPath: string[] = [];
-      previewStateContext.previewSet.forEach((v) => {
-        tmpImgUrl.push(v.previewUrl);
-        const u = new URL(v.previewUrl);
-        tmpImgPath.push(`${u.pathname}${u.search}`);
-      });
-      setImgUrl(JSON.stringify(tmpImgUrl, null, ' '));
-      setImgPath(JSON.stringify(tmpImgPath, null, ' '));
-    } catch {
-      setImgUrl('');
-      setImgPath('');
-    }
-  }, [previewStateContext.previewSet]);
-
   return (
     <Layout title="Fragment">
       <Container maxWidth="sm">
         <Box py={1}>
           <FragmentPanel groupName="Link">
-            <Box p={1}>
-              <FragmentTextField label="url" value={imgUrl} />
-            </Box>
-            <Box p={1}>
-              <FragmentTextField label="path" value={imgPath} />
-            </Box>
+            <FragmentLink />
           </FragmentPanel>
           <FragmentPanel groupName="Parameters">
             <FragmentParams />
