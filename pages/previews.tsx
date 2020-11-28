7200aa4bca88a868a9db0061fafdbd1200a96652
hankei6km <gh-hankei6km@outlook.jp>
2020-11-28 20:34:11 +0900

    Move template indicator to action bar

--- pages/previews.tsx
+++ pages/previews.tsx
@@ -4,19 +4,25 @@ import { makeStyles } from '@material-ui/core/styles';
 import Layout from '../components/Layout';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Skeleton from '@material-ui/lab/Skeleton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import PreviewContext, {
   PreviewDispatch,
   PreviewItem
 } from '../components/PreviewContext';
 import TemplatePanel from '../components/TemplatePanel';
 import ImgPreview from '../components/ImgPreview';
-import { ImportTemplateParametersSet } from '../src/template';
+import {
+  BuiltinImportTemplate,
+  ImportTemplateParametersSet
+} from '../src/template';
 
 const useStyles = makeStyles((theme) => ({
   targetIndicator: {
@@ -125,6 +131,22 @@ function SetItem({
   );
 }
 
+const useActionBarStyles = makeStyles((theme) => ({
+  bar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  indicatorOuter: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    '& .MuiButton-label > .MuiBox-root': {
+      marginLeft: theme.spacing(1)
+    }
+  }
+}));
+
 function ActionBar({
   onTemplate
 }: {
@@ -138,27 +160,64 @@ function ActionBar({
     parametersSet: ImportTemplateParametersSet;
   }) => void;
 }) {
+  const classes = useActionBarStyles();
   const previewStateContext = useContext(PreviewContext);
+  const [templateIdx, seTtemplateIdx] = useState(
+    previewStateContext.templateIdx >= 0 ? previewStateContext.templateIdx : 0
+  );
+  const [open, setOpen] = useState(false);
 
   return (
-    <TemplatePanel
-      defaultIdx={previewStateContext.templateIdx}
-      disabled={
-        previewStateContext.previewSetState === 'edited' ||
-        previewStateContext.imageBaseUrl === ''
-      }
-      onTemplate={({
-        templateIdx: idx,
-        sampleParametersSet,
-        parametersSet
-      }) => {
-        onTemplate({
-          templateIdx: idx,
-          sampleParametersSet: sampleParametersSet,
-          parametersSet: parametersSet
-        });
-      }}
-    />
+    <Box>
+      <Box className={classes.bar}>
+        <Box className={classes.indicatorOuter}>
+          <Button
+            endIcon={
+              <ExpandMoreIcon
+                style={{
+                  transform: open ? 'rotate(180deg)' : '' /*'rotate(270deg)'*/
+                }}
+              />
+            }
+            onClick={() => setOpen(!open)}
+            style={{ textTransform: 'none' }}
+          >
+            <Collapse in={!open}>
+              <Box>
+                <Typography variant="body1">
+                  {BuiltinImportTemplate[templateIdx].label}
+                </Typography>
+              </Box>
+            </Collapse>
+            <Box>
+              <Typography variant="body1">template</Typography>
+            </Box>
+          </Button>
+        </Box>
+      </Box>
+      <Box>
+        <TemplatePanel
+          defaultIdx={previewStateContext.templateIdx}
+          open={open}
+          disabled={
+            previewStateContext.previewSetState === 'edited' ||
+            previewStateContext.imageBaseUrl === ''
+          }
+          onTemplate={({
+            templateIdx: idx,
+            sampleParametersSet,
+            parametersSet
+          }) => {
+            seTtemplateIdx(idx);
+            onTemplate({
+              templateIdx: idx,
+              sampleParametersSet: sampleParametersSet,
+              parametersSet: parametersSet
+            });
+          }}
+        />
+      </Box>
+    </Box>
   );
 }
 
