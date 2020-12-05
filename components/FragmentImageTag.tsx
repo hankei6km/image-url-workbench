d5fd009cb4f02ec3178dc343433271387546d638
hankei6km <gh-hankei6km@outlook.jp>
2020-12-06 01:07:38 +0900

    Set "body1" to label in FragmentImageTag

--- components/FragmentImageTag.tsx
+++ components/FragmentImageTag.tsx
@@ -205,7 +205,7 @@ const FragmentImageTag = () => {
             aria-controls={`optional parameters panel`}
             IconButtonProps={{ edge: 'start' }}
           >
-            <Typography variant="body2">Optional fields</Typography>
+            <Typography variant="body1">Optional fields</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Box width="100%">
