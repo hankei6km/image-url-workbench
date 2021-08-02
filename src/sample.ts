42ae2a78c05e90aec35eb19bf0c4686d27455e0a
hankei6km <gh-hankei6km@outlook.jp>
2021-08-02 17:55:38 +0900

    Replace samples
    It links sample files that has been uploaded to new service(image-url-workbench).

--- src/sample.ts
+++ src/sample.ts
@@ -35,43 +35,73 @@ export type SampleImageList = SampleImage[];
 
 export const BuiltinSampleImages: SampleImageList = [
   {
-    title: 'river',
+    title: 'sample01',
     imageUrl:
-      'https://images.microcms-assets.io/protected/ap-northeast-1:9063452c-019d-4ffe-a96f-1a4524853eda/service/re-plotter/media/2020-10-24-jog1.jpg',
+      'https://images.microcms-assets.io/assets/bc4007b30bdf402f96161596bd7cbcca/34b6978634ad4938af4d475e6573d332/sample01.jpg',
     imagePaths: [],
     credit: {
       author: ['hankei6km'],
       license: {
-        id: 'CC-BY-4.0',
-        url: 'https://spdx.org/licenses/CC-BY-4.0.html#licenseText'
-      }
-    }
-  },
-  {
-    title: 'river',
-    imageUrl:
-      'https://images.microcms-assets.io/protected/ap-northeast-1:9063452c-019d-4ffe-a96f-1a4524853eda/service/re-plotter/media/2020-10-18-kaze.jpg',
-    imagePaths: []
-    // credit: {
-    //   author: ['hankei6km'],
-    //   license: {
-    //     id: 'CC-BY-4.0',
-    //     url: 'https://spdx.org/licenses/CC-BY-4.0.html#licenseText'
-    //   }
-    // }
-  },
-  {
-    title: 'railway bridge',
-    imageUrl:
-      'https://images.microcms-assets.io/protected/ap-northeast-1:9063452c-019d-4ffe-a96f-1a4524853eda/service/re-plotter/media/2020-11-03-yoko2.jpg',
-    imagePaths: [],
-    credit: {
-      author: ['hankei6km'],
-      license: {
-        id: 'CC-BY-4.0',
-        url: 'https://spdx.org/licenses/CC-BY-4.0.html#licenseText'
+        id: 'CC0-1.0',
+        url: 'https://spdx.org/licenses/CC0-1.0.html'
       },
-      webPage: 'https://hankei6km.github.io/'
+      webPage: 'https://github.com/hankei6km'
     }
-  }
+  },
+  {
+    title: 'sample02',
+    imageUrl:
+      'https://images.microcms-assets.io/assets/bc4007b30bdf402f96161596bd7cbcca/8add25df5ec24fdb9bd9ca6773201252/sample02.jpg',
+    imagePaths: [],
+    credit: {
+      author: ['hankei6km'],
+      license: {
+        id: 'CC0-1.0',
+        url: 'https://spdx.org/licenses/CC0-1.0.html'
+      },
+      webPage: 'https://github.com/hankei6km'
+    }
+  },
+  {
+    title: 'sample03',
+    imageUrl:
+      'https://images.microcms-assets.io/assets/bc4007b30bdf402f96161596bd7cbcca/437d0e523bf74ac8a7c5a87385d76330/sample03.jpg',
+    imagePaths: [],
+    credit: {
+      author: ['hankei6km'],
+      license: {
+        id: 'CC0-1.0',
+        url: 'https://spdx.org/licenses/CC0-1.0.html'
+      },
+      webPage: 'https://github.com/hankei6km'
+    }
+  },
+  {
+    title: 'sample04',
+    imageUrl:
+      'https://images.microcms-assets.io/assets/bc4007b30bdf402f96161596bd7cbcca/2ca9d04e541745ad97b0fcdce2a92764/sample04.jpg',
+    imagePaths: [],
+    credit: {
+      author: ['hankei6km'],
+      license: {
+        id: 'CC0-1.0',
+        url: 'https://spdx.org/licenses/CC0-1.0.html'
+      },
+      webPage: 'https://github.com/hankei6km'
+    }
+  },
+  {
+    title: 'sample05',
+    imageUrl:
+      'https://images.microcms-assets.io/assets/bc4007b30bdf402f96161596bd7cbcca/6d96e87dd9474049b104a04ce7df0731/sample05.jpg',
+    imagePaths: [],
+    credit: {
+      author: ['hankei6km'],
+      license: {
+        id: 'CC0-1.0',
+        url: 'https://spdx.org/licenses/CC0-1.0.html'
+      },
+      webPage: 'https://github.com/hankei6km'
+    }
+  },
 ];
